8ad618a54df890cbeb24ee8edba69ee871424696
lucvinhkhang456-source <lucvinhkhang456@gmail.com>
2026-05-09 09:38:05 +0700

    Quản lý công ty

---
+++ Volunteer.java
@@ -0,0 +1,10 @@
+public class Volunteer extends StaffMember {
+    public Volunteer(String name, String address, String phone) {
+        super(name, address, phone);
+    }
+
+    @Override
+    public double pay() {
+        return 0.0;
+    }
+}
